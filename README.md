361399cf13961bd9bce1ef5f49c6b399249bac25
alisdairjsmyth <alisdair.smyth@gmail.com>
2016-06-09 15:36:23 +1000

    Initial development

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # node-red-contrib-qmotion
-A Node-RED node to control QMotion blinds using a QSync device
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to control QMotion blinds using a QSync device
+
+Install
+-------
+Run the following command in the root directory of your Node-RED install
+    npm install node-red-contrib-qmotion
+
+Usage
+-----
+This node sends commands to a QMotion blind via a QSync device.  It is configured with the following properties:
+* IP: the IP address of the QSync device
+* debug: enables debugging within qmotion package
+
+It receives input messages containing:
+* channel: identifier of the blind
+* blindPosition: the new position of the blind
+
+The commands are submitted to the QSync device using the <a hred"https://www.npmjs.com/package/qmotion">qmotion</a> package published by <a href="https://www.npmjs.com/~devbobo">devbobo</a>.  It should be noted the qmotion package has been developed based on observations rather than a published API.  The QSync device then interacts with the blinds over 433MHz RF.
---
+++ qmotion/qmotion.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2016 Alisdair Smyth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+module.exports = function (RED) {
+    "use strict";
+
+    function validateMsg(node, msg) {
+        var validMsg = true;
+        var msgProperty = [
+            "channel",
+            "blindPosition"
+        ];
+
+        if (!(typeof msg.payload === "object")) {
+            node.error("qmotion.error.invalid-msg-payload", msg);
+            validMsg = false;
+        } else {
+            for (i in msgProperty) {
+                if (!(msgProperty[i] in msg.payload)) {
+                    node.error("qmotion.error.property-" + msgProperty[i] + "-missing", msg);
+                    validMsg = false;
+                }
+            }
+            if (validMsg) {
+                if ((typeof msg.payload.channel != "number")) {
+                    node.error("qmotion.error.invalid-channel: " + msg.payload.channel, msg);
+                    validMsg = false;
+                }
+                if ((typeof msg.payload.blindPosition != "number") ||
+                    (msg.payload.blindPosition < 0) ||
+                    (msg.payload.blindPosition > 100)) {
+                    node.error("qmotion.error.invalid-blindChannel: " + msg.payload.blindPosition, msg);
+                    validMsg = false;
+                }
+            }
+        }
+    }
+
+    function qmotion(config) {
+        RED.nodes.createNode(this, config);
+
+        var qsync = require('qmotion');
+        sync.setDebug(config.debug);
+
+        var device = new qsync(config.ip);
+
+        this.on('input', function (msg) {
+            if (validateMsg(node, msg)) {
+                var blind = device.blinds(msg.payload.channel);
+                blind.move(msg.payload.blindPosition);
+            }
+        });
+    }
+
+    RED.nodes.registerType("qmotion", qmotion);
+}
