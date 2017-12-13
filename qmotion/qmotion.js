d21bec531729f75cf9a861382e2274e1568f8e68
alisdairjsmyth <alisdair.smyth@gmail.com>
2017-12-13 23:04:41 +1100

    Prettier formatting

--- qmotion/qmotion.js
+++ qmotion/qmotion.js
@@ -13,63 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-module.exports = function (RED) {
-    "use strict";
+module.exports = function(RED) {
+  "use strict";
 
-    function validateMsg(node, msg) {
-        var validMsg = true;
-        var msgProperty = [
-            "channel",
-            "blindPosition"
-        ];
+  function validateMsg(node, msg) {
+    var validMsg = true;
+    var msgProperty = ["channel", "blindPosition"];
 
-        if (!(typeof msg.payload === "object")) {
-            node.error("qmotion.error.invalid-msg-payload", msg);
-            validMsg = false;
-        } else {
-            var i;
+    if (!(typeof msg.payload === "object")) {
+      node.error("qmotion.error.invalid-msg-payload", msg);
+      validMsg = false;
+    } else {
+      var i;
 
-            for (i in msgProperty) {
-                if (!(msgProperty[i] in msg.payload)) {
-                    node.error("qmotion.error.property-" + msgProperty[i] + "-missing", msg);
-                    validMsg = false;
-                }
-            }
-            if (validMsg) {
-                if ((typeof msg.payload.channel != "number")) {
-                    node.error("qmotion.error.invalid-channel: " + msg.payload.channel, msg);
-                    validMsg = false;
-                }
-                if ((typeof msg.payload.blindPosition != "number") ||
-                    (msg.payload.blindPosition < 0) ||
-                    (msg.payload.blindPosition > 100)) {
-                    node.error("qmotion.error.invalid-blindChannel: " + msg.payload.blindPosition, msg);
-                    validMsg = false;
-                }
-            }
+      for (i in msgProperty) {
+        if (!(msgProperty[i] in msg.payload)) {
+          node.error(
+            "qmotion.error.property-" + msgProperty[i] + "-missing",
+            msg
+          );
+          validMsg = false;
         }
+      }
+      if (validMsg) {
+        if (typeof msg.payload.channel != "number") {
+          node.error(
+            "qmotion.error.invalid-channel: " + msg.payload.channel,
+            msg
+          );
+          validMsg = false;
+        }
+        if (
+          typeof msg.payload.blindPosition != "number" ||
+          msg.payload.blindPosition < 0 ||
+          msg.payload.blindPosition > 100
+        ) {
+          node.error(
+            "qmotion.error.invalid-blindChannel: " + msg.payload.blindPosition,
+            msg
+          );
+          validMsg = false;
+        }
+      }
+    }
+  }
+
+  function qmotion(config) {
+    RED.nodes.createNode(this, config);
+    this.ip = config.ip;
+
+    var qsync = require("qmotion");
+
+    try {
+      var device = new qsync(this.ip);
+    } catch (err) {
+      this.error(err);
     }
 
-    function qmotion(config) {
-        RED.nodes.createNode(this, config);
-        this.ip    = config.ip;
+    this.on("input", function(msg) {
+      if (validateMsg(this, msg)) {
+        var blind = device.blinds(msg.payload.channel);
+        blind.move(msg.payload.blindPosition);
+      }
+    });
+  }
 
-        var qsync  = require('qmotion');
-
-        try {
-            var device = new qsync(this.ip);
-        }
-        catch (err) {
-            this.error(err);
-        }
-
-        this.on('input', function (msg) {
-            if (validateMsg(this, msg)) {
-                var blind = device.blinds(msg.payload.channel);
-                blind.move(msg.payload.blindPosition);
-            }
-        });
-    }
-
-    RED.nodes.registerType("qmotion", qmotion);
-}
+  RED.nodes.registerType("qmotion", qmotion);
+};
